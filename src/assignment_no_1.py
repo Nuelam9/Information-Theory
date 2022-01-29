011993151d785b15f14d71691d2942785cf06287
Nuelam9 <Nuelam9@github.com>
2022-01-29 18:08:11 +0100

    Removed plot part and moved to assignments_plot

--- src/assignment_no_1.py
+++ src/assignment_no_1.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3.9.5
 import os
-from lib.utils import *
 import numpy as np
-import matplotlib.pyplot as plt
+from lib.utils import *
 from lib.analysis_plot import *
 from lib.information_lib import Entropy
 
@@ -16,13 +15,7 @@ if __name__ == '__main__':
     # Computing the Entropy of a generic binary random variable
     # as a function of p.m.f. p
     entropy = list(map(Entropy, p))
-    
-    plot_settings() # for latex document
-    plt.plot(p0, entropy, 'b', lw=0.7)
-    plt.xlabel(r'$ \mathbf{p_{0}} $')
-    plt.ylabel(r'$ \mathbf{H_{2}(p_{0})\hspace{0.2}[bit]} $')
-    plt.grid()
-    plt.tight_layout()
 
     file = os.path.basename(__file__).split('.')[0]
-    plt.savefig(f'../Results/{file}/entropy_vs_p0.png', dpi=800)
+    # Save probability and entropy vectors
+    np.save(f'../Results/{file}/entropy_vs_p0.npy', np.vstack((p0, entropy)))
