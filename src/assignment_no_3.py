95c6d585cfb022aaf3bd42bc2b0a7c5adcb1798c
Nuelam9 <Nuelam9@github.com>
2022-01-29 18:21:20 +0100

    Removed plots and added saving files

--- src/assignment_no_3.py
+++ src/assignment_no_3.py
@@ -2,15 +2,19 @@
 import numpy as np
 import os
 import seaborn as sns
-from lib.information_lib import entropy, mutual_information
+from lib.information_lib import Entropy, Mutual_information
 from lib.pmf_estimation import joint_p, pmf_univariate
 from itertools import combinations
+import pandas as pd
+
 
 
 if __name__ == "__main__":
     # Load the iris dataset
     df = sns.load_dataset("iris")
-    print(df.info())
+    print('\n', df.info())
+
+    print('\n', df.head(10))
 
     # Get a copy of the dataframe 
     df_cp = df.copy()
@@ -24,10 +28,33 @@ if __name__ == "__main__":
     n = len(features)
 
     # Compute the pmf and the entropy of each features   
-    entropies = []
+    entropies, pmfs, x_vals = [], [], []
     for feature in features:
-        x_vals, pmf = pmf_univariate(df_cp[feature])  # Computing the pmf
-        entropies.append(entropy(pmf))
+        # Computing the pmf
+        x_val, pmf = pmf_univariate(df_cp[feature])  
+        # Computing the entropy
+        entropies.append(Entropy(pmf)), pmfs.append(pmf), x_vals.append(x_val)
+        
+
+    # Saving x_vals list as .csv file
+    file = os.path.basename(__file__).split('.')[0]
+    df = pd.DataFrame(x_vals, index=features).T
+    df = df.fillna(0).astype('int')
+
+    print(df.info())
+
+    df.to_csv(f'../Results/{file}/x_vals_1.csv', index=False)
+
+    # Saving pmfs list as .csv file
+    df = pd.DataFrame(pmfs, index=features).T
+    df.to_csv(f'../Results/{file}/pmfs_2.csv', index=False)
+
+    # Print pmfs
+    print('\n', df, '\n')
+
+    # Print entropies
+    print('\n', pd.DataFrame(dict(zip(features, entropies)),
+                            index=['entropy']), '\n')
 
     # Computing the Mutual information over all the features combinations
     mi_mat = np.zeros((n, n))
@@ -38,12 +65,14 @@ if __name__ == "__main__":
         py = pxy.sum(axis=0)  # Computing the y marginal probability 
         # Mutual Information is symmetric so it is enough compute only
         # the upper triangular matrix
-        mi_mat[i, j] = mutual_information(pxy, px, py)
+        mi_mat[i, j] = Mutual_information(pxy, px, py)
 
     # Make symmetric the Mutual Information matrix 
     i_lower = np.tril_indices(n, -1)
-    mi_mat[i_lower] = mi_mat.T[i_lower] 
+    mi_mat[i_lower] = mi_mat.T[i_lower]
+
+    # Print mutual information matrix
+    print(pd.DataFrame(mi_mat, columns=features, index=features))
 
     # Save the Mutual Information matrix as npy file
-    file = os.path.basename(__file__).split('.')[0]
-    np.save(f'../results/{file}/mutual_information_matrix', mi_mat)
+    np.save(f'../Results/{file}/mutual_information_matrix', mi_mat)
